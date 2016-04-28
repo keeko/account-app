1141a7e67c011870abe59796598d2d9565c6b0ad
gossi <github@gos.si>
2016-04-28 14:55:24 +0200

    keeko init

---
+++ src/AccountApplication.php
@@ -0,0 +1,21 @@
+<?php
+namespace keeko\application\account;
+
+use keeko\framework\foundation\AbstractApplication;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Keeko Account-app
+ * 
+ * @license MIT
+ * @author gossi
+ */
+class AccountApplication extends AbstractApplication {
+
+	/**
+	 * @param Request $request
+	 * @param string $path
+	 */
+	public function run(Request $request, $path) {
+	}
+}
